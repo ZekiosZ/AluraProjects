7619407f1733d14f34951dfa3a416d2c49140431
José Rafael Tejeda Mantilla <joserafaelleticia2018@gmail.com>
2025-08-07 20:59:50 -0300

    Primeira Aula JavaScript
    Primeira Aula sobre lógica de programação em javascript, util para aprender como funcionam outras linguagems usando a logica base dos programadores 😁

---
+++ Alurajavascript1/app.js
@@ -0,0 +1,20 @@
+alert("Bem-vindo ao site! do número secreto");
+numerosecreto=prompt("Escreva um número entre 0-10:");
+while(isNaN(numerosecreto) || numerosecreto < 0 || numerosecreto > 10){
+    alert("Escreva um número válido, tente novamente!");
+    numerosecreto=prompt("Escreva um número:");
+}
+numerosecreto=Number(numerosecreto);
+let numeromaquina = Math.floor(0, 10);
+while (numerosecreto != numeromaquina) {
+    alert("Que pena, você errou!");
+    numerosecreto=prompt("Tente novamente, escreva um número entre 0-10:");
+    while(isNaN(numerosecreto) || numerosecreto < 0 || numerosecreto > 10){
+        alert("Escreva um número válido, tente novamente!");
+        numerosecreto=prompt("Escreva um número:");
+    }
+}
+if (numerosecreto == numeromaquina) {
+    alert("Parabéns, você acertou o número secreto!");
+}
+
