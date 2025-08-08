9941a89d968c1f24d279b69d92082dbf31a95161
ZekiosZ <joserafaelleticia2018@gmail.com>
2025-08-08 20:17:38 -0300

    Adicionando comentarios pelo console.log para melhor monitoramento do funciomaneto do codigo

--- Alurajavascript1/app.js
+++ Alurajavascript1/app.js
@@ -1,16 +1,20 @@
 alert("Bem-vindo ao site! do número secreto");
 numerosecreto=prompt("Escreva um número entre 0-10:");
+let numeromaquina = Math.floor(Math.random() * 11);
+console.log("Número digitado" + numerosecreto, "Número certo" + numeromaquina);
 while(isNaN(numerosecreto) || numerosecreto < 0 || numerosecreto > 10){
+    console.log("Número digitado" + numerosecreto, "Número certo" + numeromaquina);
     alert("Escreva um número válido, tente novamente!");
     numerosecreto=prompt("Escreva um número:");
 }
 numerosecreto=Number(numerosecreto);
-let numeromaquina = Math.floor(Math.random() * 11);
 while (numerosecreto != numeromaquina) {
+    console.log("Número digitado" + numerosecreto, "Número certo" + numeromaquina);
     alert("Que pena, você errou!");
     numerosecreto=prompt("Tente novamente, escreva um número entre 0-10:");
     while(isNaN(numerosecreto) || numerosecreto < 0 || numerosecreto > 10){
         alert("Escreva um número válido, tente novamente!");
+        console.log("Número digitado" + numerosecreto, "Número certo" + numeromaquina);
         numerosecreto=prompt("Escreva um número:");
     }
 }
