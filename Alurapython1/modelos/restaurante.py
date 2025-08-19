89568f1a97e13423a24eafeb671f23bfb6dc47dd
ZekiosZ <joserafaelleticia2018@gmail.com>
2025-08-19 19:25:25 -0300

    Adicionando o uso de metodo construtores para facilitar a definição de atribuições a objetos

--- Alurapython1/modelos/restaurante.py
+++ Alurapython1/modelos/restaurante.py
@@ -1,19 +1,28 @@
 class Restaurante:
-    nome=""
-    categoria=""
-    ativo = False
-restaurante_praca=Restaurante()
-restaurante_praca.nome="Bistrô"
-restaurante_praca.categoria="Italiana"
-restaurante_praca.ativo=True
+    #Método Construtor, Ou função para definir parametros para os objetos
+    def __init__(self, nome, categoria):
+        self.nome=""
+        self.categoria=""
+        self.ativo = False
+restaurante_praca=Restaurante("Praça", "Bistrô")
 
-restaurante_pizza=Restaurante()
-restaurante_pizza.nome="Pizza Place"
+
+
+#Antigos testes
+'''restaurante_praca.nome="Bistrô"
+restaurante_praca.categoria="Italiana"
+restaurante_praca.ativo=True'''
+
+restaurante_pizza=Restaurante("Pizza Place", "Fast Food")
+
+
+#Antigos testes
+'''restaurante_pizza.nome="Pizza Place"
 restaurante_pizza.categoria="Fast Food"
-restaurante_pizza.ativo=True
+restaurante_pizza.ativo=True'''
 #separação
 
-categoria=Restaurante.categoria
+'''categoria=Restaurante.categoria'''
 
 #Os Testes vou colocar em comentarios
 '''if restaurante_praca.ativo==False:
@@ -21,4 +30,7 @@ categoria=Restaurante.categoria
 else:
     print("O restaurante esta ativo")
     '''
+#Print teste
 print("Nome do restaurante:",restaurante_praca.nome,"\nCategoria do restaurante:", restaurante_praca.categoria)
+#Codigo Dir antes do print ajuda a identificar possiveis comandos no objeto
+#Codigo Vars ajuda a "printar", mostrar oque esta definido dentro de determinado objeto
