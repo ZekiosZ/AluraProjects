b4acb8c80604a2a94366c74f607cef581dcc418e
ZekiosZ <joserafaelleticia2018@gmail.com>
2025-08-19 20:24:07 -0300

    Usando novos metodos construtores especiais

--- Alurapython1/modelos/restaurante.py
+++ Alurapython1/modelos/restaurante.py
@@ -1,19 +1,23 @@
 class Restaurante:
     #Método Construtor, Ou função para definir parametros para os objetos
-    def __init__(self, nome, categoria):
-        self.nome=""
-        self.categoria=""
-        self.ativo = False
-restaurante_praca=Restaurante("Praça", "Bistrô")
+    def __init__(self, nome, categoria, ativo):
+        self.nome=nome
+        self.categoria=categoria
+        self.ativo = ativo
+#self é a referência da instância que estamos usando naquele momento. Então, sempre que precisarmos da parte textual de um objeto, usaremos self.
+#E definido igual uma função, funciona parecido mas não são iguais, o __str__ e um metodo Construtor especial
+    def __str__(self):
+            return f'Restaurante de nome: {self.nome} de categoria: {self.categoria} esta atualmente: {self.ativo}'
 
 
+restaurante_praca=Restaurante("Praça", "Bistrô", "Funcionando")
 
 #Antigos testes
 '''restaurante_praca.nome="Bistrô"
 restaurante_praca.categoria="Italiana"
 restaurante_praca.ativo=True'''
 
-restaurante_pizza=Restaurante("Pizza Place", "Fast Food")
+restaurante_pizza=Restaurante("Pizza Place", "Fast Food", "Fora de serviço")
 
 
 #Antigos testes
@@ -31,6 +35,7 @@ else:
     print("O restaurante esta ativo")
     '''
 #Print teste
-print("Nome do restaurante:",restaurante_praca.nome,"\nCategoria do restaurante:", restaurante_praca.categoria)
+print(restaurante_pizza)
+print(restaurante_praca)
 #Codigo Dir antes do print ajuda a identificar possiveis comandos no objeto
 #Codigo Vars ajuda a "printar", mostrar oque esta definido dentro de determinado objeto
